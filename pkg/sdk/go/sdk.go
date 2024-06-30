827736db7e3605d06726604e86f0f828d35e016a
nyagamunene <stevenyaga2014@gmail.com>
2024-07-01 00:07:43 +0300

    Fixed search with ids
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- pkg/sdk/go/sdk.go
+++ pkg/sdk/go/sdk.go
@@ -1327,6 +1327,9 @@ func (pm PageMetadata) query() (string, error) {
 	if pm.Name != "" {
 		q.Add("name", pm.Name)
 	}
+	if pm.ID != "" {
+		q.Add("id", pm.ID)
+	}
 	if pm.Type != "" {
 		q.Add("type", pm.Type)
 	}
